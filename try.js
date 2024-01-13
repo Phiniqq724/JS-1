0332ea46694eaf8799393fb2f374409e0d13f229
phiniqq724 <fahrellnew@gmail.com>
2024-01-13 15:35:07 +0700

    hai

--- try.js
+++ try.js
@@ -26,19 +26,57 @@ document.title = "Arsadam bareng Naufal"
 
 const body = document.body
 
-body.append("Hello World");
+// body.append("Hello World");
 
-const heading1 = document.createElement('<marquee>h1</marquee>')
+// const heading1 = document.createElement('p');
 
-heading1.textContent='<marquee>Hello World</marquee>' 
+// heading1.textContent='<marquee>test</marquee>' 
 
-const namaSaya = document.createElement('p')
-namaSaya.innerHTML = '<marquee>Pikek</marquee>'
+// const namaSaya = document.createElement('p');
 
-const namaKamu = document.createElement('b')
-namaKamu.innerText = '<marquee>Naufal Dan Arsyadam (Dika)</marquee>'
+// namaSaya.innerHTML = '<marquee>Pikek</marquee>'
 
-body.append(heading1)
-body.append(namaKamu)
-body.append(namaSaya)
+// const namaKamu = document.createElement('p');
+
+// namaKamu.innerText = '<marquee>Naufal Dan Arsyadam (Dika)</marquee>'
+
+// body.append(heading1)
+// body.append(namaKamu)
+// body.append(namaSaya)
+
+const btn1 = document.getElementById('btn1')
+const btn2 = document.querySelector('.btn2')
+
+console.log(btn1)
+console.log(btn2)
+
+btn1.style.border = "none"
+
+let btnstyle = btn1.style
+
+btnstyle.padding = '8px'
+btnstyle.fontSize = '5px'
+
+function gantiWarna() {
+    alert("aman")
+    btnstyle.background = 'red'
+    const newText = document.createElement('p')
+    newText.textContent= "Im EMU OTORI"
+    body.append(newText)
+}
+
+function ubahText() {
+    btnstyle.background = 'white'
+}
+
+function munculText(){
+    const textBaru = document.createElement('p')
+    textBaru.textContent = "Tugas Gas Gas"
+    textBaru.id = "gas"
+    body.append(textBaru)
+}
+function warnaText(){
+    const tugas = document.getElementById('gas')
+    gas.style.color = "red"
+}
 
