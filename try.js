e64af17994fe5a594b1b49af6b15e1c9d0518879
phiniqq724 <fahrellnew@gmail.com>
2024-01-13 07:34:26 +0700

    iya jg

---
+++ try.js
@@ -0,0 +1,24 @@
+// // let namaGuru = []
+// // namaGuru[0] = 'Dea'
+// // namaGuru[1] = 'Fikri'
+// // namaGuru.shift()
+// // namaGuru.pop()
+// // alert(namaGuru)
+
+// const namaGuru = ['dea', 'fikri', 'pikek', 'naufal']
+
+// // For loop
+// for(let i = 0; i <namaGuru.length; i = i+1){
+//     console.log(namaGuru[i])
+// }
+let i = 0
+
+while(i < 10){
+    i++
+    console.log("Im emu otori")
+}
+
+do{
+    console.log("Im emu otori")
+    i++
+} while(i > 10)
